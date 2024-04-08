f586dbeca3ec5cfe9bedff3f8363affde54a0490
Rey Mondala <rey.mondala@ucdconnect.ie>
2024-04-08 13:40:57 +0100

    Continuation of the implementation

--- QuickSort.java
+++ QuickSort.java
@@ -37,6 +37,28 @@ public class QuickSort {
         }
     }
 
+    private static void partition(int arr[],int low,int high,double pivot) {
+        int i = low-1;
+        int j = high+1;
+
+       do {
+        while (arr[i] < pivot) {
+            i++;
+        }
+
+        while (arr[j] > pivot) {
+            j--;
+        }
+
+        if (i >= j) {
+            break;
+        }
+
+       } while(true);
+
+       swap(arr, i, j);
+    }
+
     private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
