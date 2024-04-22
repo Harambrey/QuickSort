833c3d635ed66f10188f6fb4a2ebcd58f93e06f7
Rey Mondala <rey.mondala@ucdconnect.ie>
2024-04-22 22:08:08 +0100

    Fixed arrays

--- src/QuickSortOptimized.java
+++ src/QuickSortOptimized.java
@@ -15,6 +15,7 @@ public class QuickSortOptimized {
         } else {
             // Otherwise, calculate a pivot using the calculatePivot method.
             double a = calculatePivot(arr, low, high);
+            System.out.println(a);
             // Partition the array around the pivot and get the partition index.
             int q = partition(arr, low, high, a);
 
